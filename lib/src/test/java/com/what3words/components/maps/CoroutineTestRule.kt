80eef8575ea2de39e4c3384c853de8a641897f4d
Emanuel Amiguinho <emanuel@what3words.com>
2023-02-01 14:28:47 +0000

    fixed unit tests

--- lib/src/test/java/com/what3words/components/maps/CoroutineTestRule.kt
+++ lib/src/test/java/com/what3words/components/maps/CoroutineTestRule.kt
@@ -4,14 +4,15 @@ import com.what3words.androidwrapper.helpers.DispatcherProvider
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-internal class CoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
+internal class CoroutineTestRule(val testDispatcher: TestDispatcher = StandardTestDispatcher()) :
     TestWatcher() {
 
     val testDispatcherProvider = object : DispatcherProvider {
@@ -21,14 +22,13 @@ internal class CoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = T
         override fun unconfined(): CoroutineDispatcher = testDispatcher
     }
 
-    override fun starting(description: Description?) {
+    override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(testDispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
         Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
     }
 }
