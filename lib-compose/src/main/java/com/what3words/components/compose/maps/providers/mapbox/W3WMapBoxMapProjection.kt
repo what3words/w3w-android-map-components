9da379f4af21558d5a883996381b1e2dcbc00109
Quoc Nguyen <quoc.banguyen@what3words.com>
2024-12-09 15:46:05 +0700

    Add try catch for the reflection

--- lib-compose/src/main/java/com/what3words/components/compose/maps/providers/mapbox/W3WMapBoxMapProjection.kt
+++ lib-compose/src/main/java/com/what3words/components/compose/maps/providers/mapbox/W3WMapBoxMapProjection.kt
@@ -18,7 +18,7 @@ class W3WMapBoxMapProjection(private val map: MapboxMap) : W3WMapProjection {
 
     override fun fromScreenLocation(point: PointF): W3WLatLng {
         val screenCoordinate = ScreenCoordinate(point.x.toDouble(), point.y.toDouble())
-        val mapboxPoint = map.fromScreenLocation(screenCoordinate)
+        val mapboxPoint = map.coordinateForPixel(screenCoordinate)
         return mapboxPoint.toW3WLatLng()
     }
 
@@ -26,22 +26,17 @@ class W3WMapBoxMapProjection(private val map: MapboxMap) : W3WMapProjection {
     // Reference: https://github.com/mapbox/mapbox-maps-android/issues/2131#issuecomment-2443260596
     // TODO: Check the getDeclaredField and class name every time Mapbox is updated to newer version
     private fun MapboxMap.toScreenLocation(coordinate: Point): ScreenCoordinate {
-        val field = this.javaClass.getDeclaredField("nativeMap")
-        field.isAccessible = true
-        val nativeMap = field.get(this)
-        val nativeMapClass = Class.forName("com.mapbox.maps.NativeMapImpl")
-        val method = nativeMapClass.getDeclaredMethod("pixelForCoordinate", Point::class.java)
-        method.isAccessible = true
-        return method.invoke(nativeMap, coordinate) as ScreenCoordinate
-    }
-
-    private fun MapboxMap.fromScreenLocation(screenCoordinate: ScreenCoordinate): Point {
-        val field = this.javaClass.getDeclaredField("nativeMap")
-        field.isAccessible = true
-        val nativeMap = field.get(this)
-        val nativeMapClass = Class.forName("com.mapbox.maps.NativeMapImpl")
-        val method = nativeMapClass.getDeclaredMethod("coordinateForPixel", ScreenCoordinate::class.java)
-        method.isAccessible = true
-        return method.invoke(nativeMap, screenCoordinate) as Point
+        try {
+            val field = this.javaClass.getDeclaredField("nativeMap")
+            field.isAccessible = true
+            val nativeMap = field.get(this)
+            val nativeMapClass = Class.forName("com.mapbox.maps.NativeMapImpl")
+            val method = nativeMapClass.getDeclaredMethod("pixelForCoordinate", Point::class.java)
+            method.isAccessible = true
+            return method.invoke(nativeMap, coordinate) as ScreenCoordinate
+        } catch (e: Exception) {
+            // Returns (0, 0) if something goes wrong, which will disable the recall button
+            return ScreenCoordinate(0.0, 0.0)
+        }
     }
 }
