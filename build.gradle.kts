3b026c0d79d618cb5c96373562537ba0497bb023
Duy Phạm <duypham@W3WMBP16A1044.local>
2025-01-09 15:49:00 +0700

    Upgrade Kotlin version to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.gradle.ktlint) apply false
     alias(libs.plugins.autonomousapps.analysis)
 }
